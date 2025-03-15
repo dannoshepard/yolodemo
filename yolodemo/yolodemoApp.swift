61f58569e8ffcf5fc73033e55e9a41f0067df67d
Danno <danno.shepard@gmail.com>
2025-03-15 11:58:41 -0400

    Initial Commit

---
+++ yolodemo/yolodemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  yolodemoApp.swift
+//  yolodemo
+//
+//  Created by Danno on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct yolodemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yolodemoTests/yolodemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  yolodemoTests.swift
+//  yolodemoTests
+//
+//  Created by Danno on 3/15/25.
+//
+
+import Testing
+@testable import yolodemo
+
+struct yolodemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
